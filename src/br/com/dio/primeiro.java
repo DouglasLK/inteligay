b0886e4e64f0a08ec7fcd779f20999100fecd209
douglas <dougsdl7@gmail.com>
2021-08-24 13:39:25 -0300

    inteligaaaay

---
+++ src/br/com/dio/primeiro.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class primeiro {
+    public static void main(String[] args) {
+        gato gato = new gato();
+
+        System.out.println(gato);
+
+//        int a = 5;
+//        int b = 3;
+//        System.out.println("quem leu e gay");
+//        System.out.println(a * b);
+    }
+}
